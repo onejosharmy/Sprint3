a0c33d72511d7be99f69158d5b0dc7f81e61d969
Joshua Collins <jcollin4@my.westga.edu>
2018-02-16 11:43:10 -0500

    Added launcher icon and layout/code from MusicPlayer

--- app/src/main/java/media/cs4985/westga/edu/audiocontroller/MainActivity.java
+++ app/src/main/java/media/cs4985/westga/edu/audiocontroller/MainActivity.java
@@ -2,6 +2,7 @@ package media.cs4985.westga.edu.audiocontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView view = findViewById(R.id.musicName);
+        view.setSelected(true);
     }
 }
